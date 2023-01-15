7367280d15d543ee7bd755ec135710357a0ec091
ธนาโชค สุวรรณ์ 653450287-7 <119285237+boss443@users.noreply.github.com>
2023-01-15 21:50:38 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace Work
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxMax = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxFaclty = new System.Windows.Forms.TextBox();
+            this.textBoxGrade = new System.Windows.Forms.TextBox();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.textBoxBirth = new System.Windows.Forms.TextBox();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxMin = new System.Windows.Forms.TextBox();
+            this.textBoxPeople = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Birthyear = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Height = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Grade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Faculty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxMax
+            // 
+            this.textBoxMax.Location = new System.Drawing.Point(345, 308);
+            this.textBoxMax.Name = "textBoxMax";
+            this.textBoxMax.Size = new System.Drawing.Size(100, 23);
+            this.textBoxMax.TabIndex = 23;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name,
+            this.ID,
+            this.Birthyear,
+            this.Height,
+            this.Grade,
+            this.Faculty});
+            this.dataGridView1.Location = new System.Drawing.Point(270, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(645, 226);
+            this.dataGridView1.TabIndex = 22;
+            // 
+            // textBoxFaclty
+            // 
+            this.textBoxFaclty.Location = new System.Drawing.Point(135, 255);
+            this.textBoxFaclty.Name = "textBoxFaclty";
+            this.textBoxFaclty.Size = new System.Drawing.Size(100, 23);
+            this.textBoxFaclty.TabIndex = 21;
+            // 
+            // textBoxGrade
+            // 
+            this.textBoxGrade.Location = new System.Drawing.Point(135, 215);
+            this.textBoxGrade.Name = "textBoxGrade";
+            this.textBoxGrade.Size = new System.Drawing.Size(100, 23);
+            this.textBoxGrade.TabIndex = 20;
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Location = new System.Drawing.Point(135, 172);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(100, 23);
+            this.textBoxHeight.TabIndex = 19;
+            // 
+            // textBoxBirth
+            // 
+            this.textBoxBirth.Location = new System.Drawing.Point(135, 132);
+            this.textBoxBirth.Name = "textBoxBirth";
+            this.textBoxBirth.Size = new System.Drawing.Size(100, 23);
+            this.textBoxBirth.TabIndex = 18;
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(135, 92);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(100, 23);
+            this.textBoxId.TabIndex = 17;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(135, 52);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 23);
+            this.textBoxName.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 15);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Height";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(54, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 15);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Grade";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(55, 264);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 15);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Faculty";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 15);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Birth Year";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 15);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Name";
+            // 
+            // textBoxMin
+            // 
+            this.textBoxMin.Location = new System.Drawing.Point(528, 308);
+            this.textBoxMin.Name = "textBoxMin";
+            this.textBoxMin.Size = new System.Drawing.Size(100, 23);
+            this.textBoxMin.TabIndex = 30;
+            // 
+            // textBoxPeople
+            // 
+            this.textBoxPeople.Location = new System.Drawing.Point(699, 308);
+            this.textBoxPeople.Name = "textBoxPeople";
+            this.textBoxPeople.Size = new System.Drawing.Size(100, 23);
+            this.textBoxPeople.TabIndex = 31;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(165, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 33);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(43, 322);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 33);
+            this.button2.TabIndex = 33;
+            this.button2.Text = "Cler";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(362, 340);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 15);
+            this.label7.TabIndex = 34;
+            this.label7.Text = "MaxGrade";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(548, 340);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 15);
+            this.label8.TabIndex = 35;
+            this.label8.Text = "MinGrade";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(716, 340);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 15);
+            this.label9.TabIndex = 36;
+            this.label9.Text = "All people";
+            // 
+            // Name
+            // 
+            this.Name.DataPropertyName = "Name";
+            this.Name.HeaderText = "Name";
+            this.Name.Name = "Name";
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "ID";
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // Birthyear
+            // 
+            this.Birthyear.DataPropertyName = "Birthyear";
+            this.Birthyear.HeaderText = "Birth Year";
+            this.Birthyear.Name = "Birthyear";
+            // 
+            // Height
+            // 
+            this.Height.DataPropertyName = "Height";
+            this.Height.HeaderText = "Height";
+            this.Height.Name = "Height";
+            // 
+            // Grade
+            // 
+            this.Grade.DataPropertyName = "Grade";
+            this.Grade.HeaderText = "Grade";
+            this.Grade.Name = "Grade";
+            // 
+            // Faculty
+            // 
+            this.Faculty.DataPropertyName = "Faculty";
+            this.Faculty.HeaderText = "Faclty";
+            this.Faculty.Name = "Faculty";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1120, 564);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxPeople);
+            this.Controls.Add(this.textBoxMin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxMax);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBoxFaclty);
+            this.Controls.Add(this.textBoxGrade);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.textBoxBirth);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.textBoxName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBoxMax;
+        private DataGridView dataGridView1;
+        private TextBox textBoxFaclty;
+        private TextBox textBoxGrade;
+        private TextBox textBoxHeight;
+        private TextBox textBoxBirth;
+        private TextBox textBoxId;
+        private TextBox textBoxName;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBoxMin;
+        private TextBox textBoxPeople;
+        private Button button1;
+        private Button button2;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private DataGridViewTextBoxColumn Name;
+        private DataGridViewTextBoxColumn ID;
+        private DataGridViewTextBoxColumn Birthyear;
+        private DataGridViewTextBoxColumn Height;
+        private DataGridViewTextBoxColumn Grade;
+        private DataGridViewTextBoxColumn Faculty;
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work
+{
+    internal class Student
+    {
+        private string _name;
+        private string _id;
+        private int _birthYear;
+        private int _height;
+        private double _grade;
+        private string _faculty;
+        private string name;
+        private string id;
+        private double iYear;
+        private int iheight;
+        private double igrade;
+        private string faculty;
+
+        public Student(string name, string id, int birthYear, int height, double grade, string faculty)
+        {
+            this._name = name;
+            this._id = id;
+            this._birthYear = birthYear;
+            this._height = height;
+            this._grade = grade;
+            this._faculty = faculty;
+        }
+
+        public Student(string name, string id, double iYear, int iheight, double igrade, string faculty)
+        {
+            this.name = name;
+            this.id = id;
+            this.iYear = iYear;
+            this.iheight = iheight;
+            this.igrade = igrade;
+            this.faculty = faculty;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+        public string ID
+        {
+            get { return _id; }
+        }
+        public int BirthYear
+        {
+            get { return _birthYear; }
+        }
+        public int Height
+        {
+            get { return _height; }
+        }
+        public double Grade
+        {
+            get { return _grade; }
+        }
+        public string Faculty
+        {
+            get { return _faculty; }
+        }
+        public int myAge()
+        {
+            int age = 2023 - _birthYear;
+            return age;
+        }
+    }
+}
